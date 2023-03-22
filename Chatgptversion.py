65250d696823996ade23b86eb68067dfc60d516c
Fayzulloh <127090498+Fayzullohon@users.noreply.github.com>
2023-03-22 23:25:30 +0500

    ChatGPT version

---
+++ Chatgptversion.py
@@ -0,0 +1,34 @@
+import openai
+
+# OpenAI API keyni o'rnatamiz
+openai.api_key = 'API kalit kirgiziladi'
+
+# Foydalanayotgan modelni aniqlaymiz
+engine = 'text-davinci-002'
+
+# Suhbatlashish funksiyasini yaratamiz
+def chat_with_gpt(prompt):  
+    # GPT-3 API orqali suhbatlashamiz
+    response = openai.Completion.create(
+        engine=engine,
+        prompt=prompt,
+        max_tokens=60, # ko'proq so'zlar uchun qiymatni o'zgartiramiz
+        n=1,
+        stop="-EOF-", # to'xtatish belgisini qo'yamiz
+        temperature=0.7, # javobning yorqinligini o'zgartiramiz
+    )
+
+    # API javobini olish
+    message = response.choices[0].text
+    return message.strip()
+
+# Foydalanuvchidan keladigan savollarni qabul qilish
+while True:
+    prompt = input('Siz: ')
+
+    # Suhbatlashish funksiyasini chaqirib, javobni olish
+    response = chat_with_gpt(prompt)
+
+    # GPT-3 javobini chiqaramiz
+    print('AI: ' + response)
+
